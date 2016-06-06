ce0019e557167c565687db079594a95245436bbc
benjaminfs <h1n1china@163.com>
2016-06-06 17:12:12 +0800

    0606

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -19,4 +19,6 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^article/', include("article.urls")),
+    url(r'^$', 'article.views.article_list', name='article_list'),
 ]
